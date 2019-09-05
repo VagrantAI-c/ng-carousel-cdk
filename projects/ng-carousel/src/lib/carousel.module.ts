ff5a78e14746d6af383880dcbf8a50c358a6245c
kirill-chirkov-at-clouty <kirill.chirkov@clouty.ru>
2019-09-05 12:59:29 +0300

    fix: remove redundant animations module

--- projects/ng-carousel/src/lib/carousel.module.ts
+++ projects/ng-carousel/src/lib/carousel.module.ts
@@ -11,7 +11,6 @@ import { CarouselEngineComponent } from './private/views/carousel-engine.compone
 @NgModule({
     imports: [
         CommonModule,
-        BrowserAnimationsModule,
         A11yModule,
     ],
     declarations: [
