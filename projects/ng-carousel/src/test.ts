a056bdd05c2e7aef9f420ad09baf3ed0b9f9074b
kirill.chirkov@home <kirill.chirkov@outlook.com>
2021-07-06 11:35:26 +0300

    chore: remove hammerjs dependency

--- projects/ng-carousel/src/test.ts
+++ projects/ng-carousel/src/test.ts
@@ -4,7 +4,6 @@ import 'zone.js/dist/zone';
 import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
-import 'hammerjs';
 
 declare const require: any;
 
