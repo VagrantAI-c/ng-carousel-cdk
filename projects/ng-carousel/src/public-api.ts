29c99acc1410b20cd06e63666e567ada1501e422
kirill.chirkov@home <kirill.chirkov@outlook.com>
2022-02-10 15:58:01 +0300

    fix missing export

--- projects/ng-carousel/src/public-api.ts
+++ projects/ng-carousel/src/public-api.ts
@@ -8,4 +8,5 @@ export * from './lib/carousel-config.type';
 export * from './lib/carousel-width-mode';
 export * from './lib/carousel-align-mode';
 export * from './lib/carousel-slide.directive';
+export * from './lib/prevent-ghost-click.module';
 export * from './lib/prevent-ghost-click.directive';
